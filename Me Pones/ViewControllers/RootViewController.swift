89cbe83cc1530ac42534754724b020a94ac72e56
Mansour <theboom.kajas@gmail.com>
2024-09-18 12:01:16 +0200

    Ya tenemos Home (TabBarController) y la hemos separado en un Storyboard diferente por organización y limpieza

---
+++ Me Pones/ViewControllers/RootViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RootViewController.swift
+//  Me Pones
+//
+//  Created by Mañanas on 18/9/24.
+//
+
+import UIKit
+
+class RootViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // Disable back button
+        self.navigationItem.setHidesBackButton(true, animated: false)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Hide navigation bar
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Show navigation bar
+        self.navigationController?.isNavigationBarHidden = false
+    }
+
+}
